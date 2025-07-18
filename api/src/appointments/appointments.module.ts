1fa462214f671ce44da59b4caabbce9eee3c461e
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2025-07-17 23:58:48 -0400

    API USER! OK!

--- api/src/appointments/appointments.module.ts
+++ api/src/appointments/appointments.module.ts
@@ -1,9 +1,15 @@
+// src/appointments/appointments.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Appointment } from './entities/appointment.entity';
 import { AppointmentsController } from './appointments.controller';
 import { AppointmentsService } from './appointments.service';
+import { User } from '../users/entities/user.entity'; // Asegúrate de importar User
 
 @Module({
+  // V E R I F I C A   E S T A   L Í N E A
+  imports: [TypeOrmModule.forFeature([Appointment, User])],
   controllers: [AppointmentsController],
-  providers: [AppointmentsService]
+  providers: [AppointmentsService],
 })
 export class AppointmentsModule {}
