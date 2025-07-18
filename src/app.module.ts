4ca0e60d211cfa956f4cb7fdef4cfc9709d6715b
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2025-07-17 21:59:50 -0400

    feat(core): Configurar la conexión de la base de datos y los módulos/entidades principales

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables de entorno estén disponibles en todo el proyecto
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'user',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_DATABASE || 'appointments_db',
+      autoLoadEntities: true, // Carga automáticamente las entidades que definamos
+      synchronize: true, // ¡OJO! Sincroniza el schema. Solo para desarrollo.
+    }),
+    UsersModule,
+    AppointmentsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
